e0b7915c7ae945fe62fa607bac2d2ed9a3b86882
Stephen Cox <stephen_cox@me.com>
2018-04-07 15:24:28 +0800

    Changed ipsum text

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+//All Lorem text from pan-ipsum
+
 let chilliText = `<h2 class="text-center">Chilli Con Carne</h2>
 <div class="recipe-text">
   <h3 class="text-center">Ingredients</h3>
@@ -14,7 +16,7 @@ let chilliText = `<h2 class="text-center">Chilli Con Carne</h2>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>`;
-
+//Classic Lorem
 
 document.getElementById("recipe-chilli").addEventListener("click", replaceChilli);
 
@@ -22,23 +24,28 @@ function replaceChilli() {
     document.getElementById("recipe-template").innerHTML = chilliText;
 }
 
+document.getElementById("goHome").addEventListener("click", reLoadHome);
+
+function reLoadHome() {
+    document.getElementById("recipe-template").innerHTML = chilliText;
+}
+
 let felafalText = `<h2 class="text-center">Felafal</h2>
 <div class="recipe-text">
   <h3 class="text-center">Ingredients</h3>
   <ul class="recipe-ingredients">
-    <li>Lorem ipsum dolor sit amet</li>
-    <li>consectetur adipisicing elit</li>
-    <li>sed do eiusmod tempor incididunt</li>
-    <li>ut labore et dolore magna aliqua</li>
-    <li>Ut enim ad minim veniam</li>
-    <li>quis nostrud exercitation</li>
-    <li>ullamco laboris nisi</li>
+    <li>Bachha' biq cha'par ghang ghangwi' ghiq ghuy'cha' jajvam</li>
+    <li>er nahjej naq ngusdi' pa' pur</li>
+    <li>qan qarghan ros sa'hut tijwi'ghom yur 'ur</li>
+    <li>Cha'par cha'qu' chevwi'</li>
+    <li>tlhoy' denib qatlh ghet holqed homwi'</li>
+    <li>jaghla' lolchu' ma'veq ngech</li>
+    <li>petaq pivchem qevas qi'tu' tangqa' tennus</li>
   </ul>
   <h3 class="text-center">Instructions</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>todduj tus wo'riv 'orghengan. Chevwi' tlhoy' choq choq da'vi' hos chohwi' hu huchqed juch marwi' megh'an neslo' ngoqde' qa'meh qa'rol qen qa'hom rech tagha' tey' tiq tlhepqe' viq 'edsehcha 'em 'orghen 'orghenya' 'ud'a' 'usgheb. Bachha' boqha''egh bo'degh denib denibya' qatlh ghang ghaytanha' nan qay qa'ri' qo'qad 'edsehcha 'iqnah. Be'etor bireqtagh boq borghel chaw' cheb da'vi' dugh law' nanwi' natlh nuh bey' ngusdi' pipyus qanrad qep'it qongdaqdaq rav'eq rutlh tlhoy yem 'ev.</p>
 </div>`;
-
+//Klingon Lorem
 
 document.getElementById("recipe-felafal").addEventListener("click", replaceFelafal);
 
@@ -50,19 +57,18 @@ let pastaText = `<h2 class="text-center">Pasta With Tomato Sauce</h2>
 <div class="recipe-text">
   <h3 class="text-center">Ingredients</h3>
   <ul class="recipe-ingredients">
-    <li>Lorem ipsum dolor sit amet</li>
-    <li>consectetur adipisicing elit</li>
-    <li>sed do eiusmod tempor incididunt</li>
-    <li>ut labore et dolore magna aliqua</li>
-    <li>Ut enim ad minim veniam</li>
-    <li>quis nostrud exercitation</li>
-    <li>ullamco laboris nisi</li>
+    <li>A functioning police state needs no police</li>
+    <li>silence is only frightening to people who are compulsively verbalizing</li>
+    <li>a paranoid is someone who knows a little of what's going on</li>
+    <li>there is no intensity of love or feeling that does not involve the risk of crippling hurt</li>
+    <li>it is a duty to take this risk, to love and feel without defense or reserve</li>
+    <li>i don't care if people hate my guts</li>
+    <li>i assume most of them do</li>
   </ul>
   <h3 class="text-center">Instructions</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>The important question is whether they are in a position to do anything about it. love? what is it? most natural painkiller what there is. whether you like it or not, you are committed to the human endeavor. i cannot ally myself with such a purely negative goal as avoidance of suffering. suffering is a chance you take by the fact of being alive. in the magical universe there are no coincidences and there are no accidents. nothing happens unless someone wills it to happen. there is in fact something obscene and sinister about photography, a desire to imprison, to incorporate, a sexual intensity of pursuit. he thought this was cute at first in the morning with transparentjelly... would have amputated spontaneously how long does it take man to realize that he cannot want what he wants? you have to live in hell to see heaven.</p>
 </div>`;
-
+//William Burroughs Lorem
 
 document.getElementById("recipe-pasta").addEventListener("click", replacePasta);
 
@@ -74,19 +80,18 @@ let guacamoleText = `<h2 class="text-center">Guacamole</h2>
 <div class="recipe-text">
   <h3 class="text-center">Ingredients</h3>
   <ul class="recipe-ingredients">
-    <li>Lorem ipsum dolor sit amet</li>
-    <li>consectetur adipisicing elit</li>
-    <li>sed do eiusmod tempor incididunt</li>
-    <li>ut labore et dolore magna aliqua</li>
-    <li>Ut enim ad minim veniam</li>
-    <li>quis nostrud exercitation</li>
-    <li>ullamco laboris nisi</li>
+    <li>Beer croakies</li>
+    <li>crushed dude</li>
+    <li>fistpump</li>
+    <li>frathard gtl</li>
+    <li>hookup lasers</li>
+    <li>lax mad mixer</li>
+    <li>ragefest</li>
   </ul>
   <h3 class="text-center">Instructions</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>Rageout shades sorority chicks sperries talking tan that was dope. Awesome beer bicep curls brodown brosky burberry deep dumbbells facemelt hella i'd hit it keg party light mcgotes metbrosexual mixer natty quads salmon colored polo solid sweetness talking this party is lame throw a rager v neckbro. Brodown bromo burberry crib cruisin' dave matthews band fratastic hella hookup jager bomb ladies lax noxplode ragefest rageout redbull vodka sickness squash sweetness talking tony. Brotally crib deep double popped collar epic fistpump frisbee hit me up kicks noxplode ragefest salmon colored polo searsucker spank squash totally visor</p>
 </div>`;
-
+//Bro Ipsum
 
 document.getElementById("recipe-guac").addEventListener("click", replaceGuac);
 
@@ -98,19 +103,18 @@ let spiceMixText = `<h2 class="text-center">Chilli Spice Mix</h2>
 <div class="recipe-text">
   <h3 class="text-center">Ingredients</h3>
   <ul class="recipe-ingredients">
-    <li>Lorem ipsum dolor sit amet</li>
-    <li>consectetur adipisicing elit</li>
-    <li>sed do eiusmod tempor incididunt</li>
-    <li>ut labore et dolore magna aliqua</li>
-    <li>Ut enim ad minim veniam</li>
-    <li>quis nostrud exercitation</li>
-    <li>ullamco laboris nisi</li>
+    <li>The un-cola</li>
+    <li>empowering people afore ye go</li>
+    <li>the ultimate driving experience</li>
+    <li>bring out the branston</li>
+    <li>when you say budweiser you've said it all</li>
+    <li>your best bet for a fuller flavour</li>
+    <li>internationally acknowledged to be the finest cigarette in the world</li>
   </ul>
   <h3 class="text-center">Instructions</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>Ready when you are we all adore a kia-ora life's good coffee at its best. Empowering people fly the american way making life richer for the pourer barq's has bite the united colors of benetton sweet as the moment when the pod went 'pop' always coca-cola for your throat's sake smoke craven 'a' - they never vary where the rubber meets the road georgia on my mind golden flakeit's where you find the flavour give him a right good hemeling tonight snap crackle pop we all adore a kia-ora body odour don't live a little live a lotto the curious county.</p>
 </div>`;
-
+// Advetising Slogans ipsum
 
 document.getElementById("recipe-spicemix").addEventListener("click", replaceSpiceMix);
 
